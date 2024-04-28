9bdbea2a7f86272535f635cb1b10c358cbdae638
Cheetah0x <niallcheetham@gmail.com>
2024-04-28 15:30:58 +0200

    sign-in

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,11 +1,13 @@
 
 //the login page
 import Login from "../components/login";
+import Navbar from "../components/navbar";
 
 export default function Attest() {
     return (
       <>
       <main>
+        <Navbar />
         <Login />
       </main>
       </>
