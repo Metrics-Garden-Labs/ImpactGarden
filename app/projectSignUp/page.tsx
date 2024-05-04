60353055aa6e054e7c0bfd955eb120cc0f52f539
Cheetah0x <niallcheetham@gmail.com>
2024-05-04 16:20:38 +0100

    register ui

--- app/projectSignUp/page.tsx
+++ app/projectSignUp/page.tsx
@@ -18,6 +18,9 @@ import Link from 'next/link';
 import FarcasterLogin from '../components/farcasterLogin';
 import useLocalStorage from '@/src/hooks/use-local-storage-state';
 import ReCAPTCHA from 'react-google-recaptcha';
+import { FaXTwitter } from "react-icons/fa6";
+import { FaGithub } from "react-icons/fa";
+import { BsGlobe2 } from "react-icons/bs";
 
 type AttestationData = {
   projectName: string;
@@ -56,6 +59,7 @@ export default function AttestDb() {
   const [imageUrl, setImageUrl] = useState<string>('');
   const [ecosystem, setEcosystem] = useState<string>('Optimism');
   const [ isLoading, setIsLoading ] = useState<boolean>(false);
+  const [isPreview, setIsPreview] = useState<boolean>(false);
   console.log('Ecosystem', ecosystem);
   console.log('walletAddress', walletAddress);
   console.log('Fid', fid);
@@ -67,6 +71,11 @@ export default function AttestDb() {
     const selectedValue = e.target.value as AttestationNetworkType;
     handleNetworkChange(selectedValue);
     console.log('Selected Network', selectedValue);
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setAttestationData((prevData) => ({ ...prevData, [name]: value }));
   };
 
   const handleEcosystemChangeEvent = (e: React.ChangeEvent<HTMLSelectElement>) => {
@@ -82,6 +91,17 @@ export default function AttestDb() {
       [name]: value,
     }));
   };
+
+  const handleNext = () => {
+    // Ensure required fields are filled before allowing a preview
+    if (!attestationData.projectName || !ecosystem) {
+      alert('Please fill in required fields.');
+      return;
+    }
+    setIsPreview(true);
+  };
+
+  const handleBackToEdit = () => setIsPreview(false);
 
     //Captcha logic
   const onSubmit = async (event: FormEvent) => {
@@ -248,160 +268,271 @@ export default function AttestDb() {
   };
 
   return (
-    <div className='bg-white text-black'>
+    <div className="min-h-screen flex flex-col bg-white text-black">
       <Navbar />
-      <div className="min-h-screen w-full flex justify-center relative items-center">
-        <div className='absolute right-4 top-4'>
-        {user.fid ? (
-            <ConnectButton />
-          ) : (
-            <FarcasterLogin />
-          )}
+      <div className="flex justify-center relative w-full mt-10 px-8">
+        {/* Left Column */}
+        { isPreview ? (
+
+        <div className="w-1/4 pr-8">
+          <h1 className="font-bold text-2xl">Register a project</h1>
+          <p className="text-gray-600 mt-2">Project preview & confirmation</p>
         </div>
-        <form>
-          <h1 className="text-black">EAS, need to be logged in, fixed schema is optimism</h1>
+        ) : (
+          <div className="w-1/4 pr-8 flex flex-col items-center">
+               <div>
+                 <h1 className="font-bold text-2xl text-center">Register a project</h1>
+                 <p className="text-gray-600 mt-2 text-center">Tell us more about your project</p>
+                 <h2 className="font-semibold mt-10 pb-10 text-center text-lg">Project card preview</h2>
+          
+                 <div className="shadow-2xl rounded mx-auto mt-6">
+                   <div className="pt-6 pb-6">
+                   {imageUrl ? (
+                    <Image
+                      src={imageUrl}
+                      alt="Project Logo"
+                      width={100}
+                      height={100}
+                      className="mx-auto object-contain"
+                    />
+                  ) : (
+                    <div className="mx-auto w-32 h-32 bg-gray-300 rounded-md flex items-center justify-center">
+                    </div>
+                  )}
+                     <h3 className="text-center mt-2 font-semibold text-gray-500">
+                       {attestationData.projectName || 'Project name'}
+                     </h3>
+                     <div className="flex justify-center py-4 items-center">
+                       <BsGlobe2 className="text-black mx-2 text-lg" />
+                       <FaXTwitter className="text-black mx-2 text-lg" />
+                       <FaGithub className="text-black mx-2 text-lg" />
+                     </div>
+                   </div>
+                 </div>
+               </div>
+               </div>
+        )}
 
-          <div className="sm:col-span-4 p-3">
-            <label htmlFor="chain" className="block text-sm font-medium leading-6 text-gray-900">
-              Select Attestation Network
-            </label>
-            <div className="mt-2">
-              <select
-                id="attestationChain"
-                name="attestationChain"
-                value={selectedNetwork}
-                onChange={handleNetworkChangeEvent}
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-              >
-                {Object.keys(networkContractAddresses).map((network) => (
-                  <option key={network} value={network}>
-                    {network}
-                  </option>
-                ))}
-              </select>
+        {/* Center Column */}
+        {isPreview ? (
+          <div className="w-2/3 bg-white p-8 shadow-lg rounded mx-auto">
+          <h2 className="font-semibold mt-6 text-center text-lg">Project card preview</h2>
+          <div className="shadow-2xl rounded mx-auto mt-6 pt-8 pb-8 w-1/2 flex flex-col items-center">
+            {imageUrl ? (
+              <Image
+                src={imageUrl}
+                alt="Project Logo"
+                width={200}
+                height={200}
+                className="object-contain"
+              />
+            ) : (
+              <div className="w-48 h-48 bg-gray-300 rounded-md flex items-center justify-center">
+                {/* Add optional placeholder content if needed */}
+              </div>
+            )}
+            <h3 className="text-center mt-6 mb-6 font-semibold text-gray-500">
+              {attestationData.projectName || 'Project name'}
+            </h3>
+            <div className="flex justify-center py-4 items-center">
+              <BsGlobe2 className="text-black mx-2 text-lg" />
+              <FaXTwitter className="text-black mx-2 text-lg" />
+              <FaGithub className="text-black mx-2 text-lg" />
             </div>
           </div>
 
-          <div className="sm:col-span-4 p-3">
-            <label htmlFor="chain" className="block text-sm font-medium leading-6 text-gray-900">
-              What ecosystem is your project contributing to?
-            </label>
-            <div className="mt-2">
-              <select
-                id="ecosystem"
-                name="ecosystem"
-                value={ecosystem}
-                onChange={handleEcosystemChangeEvent}
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-              >
-                {networks.map((network) => (
-                  <option key={network} value={network}>
-                    {network} Ecosystem
-                  </option>
-                ))}
-              </select>
-            </div>
+          <div className="mt-20 mb-20  flex justify-center w-full">
+            <ReCAPTCHA sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!} onChange={setCaptcha} />
           </div>
 
-          <div className="p-3">
-            <h3>What is the name of your project?</h3>
+          <div className="flex justify-center space-x-6 mt-20 mb-20">
+            <button
+              className="px-4 py-2 w-1/5 text-sm font-medium text-white bg-black rounded-md shadow-sm hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              type="button"
+              onClick={createAttestation}
+            >
+              Confirm & Attest
+            </button>
+            <button
+              className="px-4 py-2 w-1/5 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              type="button"
+              onClick={handleBackToEdit}
+            >
+              Back to Edit
+            </button>
+          </div>
+        </div>
+        
+        ) : (
+          <form className="w-1/3 bg-white p-6 shadow rounded space-y-6">
+        <div>
+          <label htmlFor="attestationChain" className="block text-sm font-medium leading-6 text-gray-900">
+            Select Attestation Network *
+          </label>
+          <div className="mt-2">
+            <select
+              id="attestationChain"
+              name="attestationChain"
+              value={selectedNetwork}
+              onChange={handleNetworkChangeEvent}
+              className="block w-full rounded-md border-0 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-700 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            >
+              {Object.keys(networkContractAddresses).map((network) => (
+                <option key={network} value={network}>
+                  {network}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+
+          <div>
+          <label htmlFor="ecosystem" className="block text-sm font-medium leading-6 text-gray-900">
+            What ecosystem is your project contributing to? *
+          </label>
+          <div className="mt-2">
+            <select
+              id="ecosystem"
+              name="ecosystem"
+              value={ecosystem}
+              onChange={handleEcosystemChangeEvent}
+              className="block w-full rounded-md border-0 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-700 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            >
+              {networks.map((network) => (
+                <option key={network} value={network}>
+                  {network} Ecosystem
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+
+          <div>
+          <label htmlFor="projectName" className="block text-sm font-medium leading-6 text-gray-900">
+            What is the name of your project? *
+          </label>
+          <div className="mt-2">
             <input
               type="text"
-              placeholder="Type Project Name here"
+              id="projectName"
               name="projectName"
               value={attestationData.projectName}
               onChange={handleAttestationChange}
-              className="input input-bordered w-full max-w-xs"
+              className="block w-full rounded-md border-0 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-700 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="Type Project Name here"
             />
           </div>
+        </div>
 
-          <div className="p-3">
-            <h3>What is the website URL of your project?</h3>
+        <div>
+          <label htmlFor="websiteUrl" className="block text-sm font-medium leading-6 text-gray-900">
+              <span>What is the website URL of your project? </span>
+              <span className="text-gray-500 text-sm">(Optional)</span>
+              
+          </label>
+          <div className="mt-2">
             <input
               type="text"
-              placeholder="Type the website URL here"
+              id="websiteUrl"
               name="websiteUrl"
               value={attestationData.websiteUrl}
               onChange={handleAttestationChange}
-              className="input input-bordered w-full max-w-xs"
+              className="block w-full rounded-md border-0 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-700 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="Type the website URL here"
             />
           </div>
+        </div>
 
-          <div className="p-3">
-            <h3>Please Input your Twitter URL</h3>
+        <div>
+          <label htmlFor="twitterUrl" className="block text-sm font-medium leading-6 text-gray-900">
+            <span>What is the Twitter URL of your project? </span>
+            <span className="text-gray-500 text-sm">(Optional)</span>
+          </label>
+          <div className="mt-2">
             <input
               type="text"
-              placeholder="Type your Twitter URL here"
+              id="twitterUrl"
               name="twitterUrl"
               value={attestationData.twitterUrl}
               onChange={handleAttestationChange}
-              className="input input-bordered w-full max-w-xs"
+              className="block w-full rounded-md border-0 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-700 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="Type the Twitter URL here"
             />
           </div>
+        </div>
 
-          <div className="p-3">
-            <h3>Please input the Github URL of your Project</h3>
+        <div>
+          <label htmlFor="githubURL" className="block text-sm font-medium leading-6 text-gray-900">
+            <span>What is the Github URL of your project? </span>
+            <span className="text-gray-500 text-sm">(Optional)</span>
+          </label>
+          <div className="mt-2">
             <input
               type="text"
-              placeholder="Type your Github URL here"
+              id="githubURL"
               name="githubURL"
               value={attestationData.githubURL}
               onChange={handleAttestationChange}
-              className="input input-bordered w-full max-w-xs"
+              className="block w-full rounded-md border-0 py-2.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-700 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              placeholder="Type the Github URL here"
             />
           </div>
+        </div>
 
-          <h2>Please upload the logo of your project</h2>
+          <h2>Please upload the logo of your project *</h2>
 
           {imageUrl ? (
-            <Image
-              src={imageUrl}
-              alt="Logo of the project"
-              width={1000}
-              height={667}
-              className="w-full h-64 object-contain"
-            />
-          ) : (
-            <UploadDropzone
-              className="border-black bg-slate-300 ut-label:text-lg ut-allowed-content:ut-uploading:text-red-300"
-              
-              endpoint="imageUploader"
-              onClientUploadComplete={(res) => {
-                setImageUrl(res[0].url);
-                console.log("Files: ", res);
-                console.log("Uploaded Image URL:", res[0].url);
-                console.log(setImageUrl);
-                alert("Upload Completed");
-              }}
-              onUploadError={(error) => {
-                alert(`ERROR! ${error.message}`);
-              }}
-            />
-          )}
+          <Image
+            src={imageUrl}
+            alt="Logo of the project"
+            width={1000}
+            height={667}
+            className="w-full h-64 object-contain"
+          />
+        ) : (
+          <UploadDropzone
+            className="border-black bg-slate-300 w-full h-64 ut-label:text-lg ut-allowed-content:ut-uploading:text-red-300"
+            endpoint="imageUploader"
+            onClientUploadComplete={(res) => {
+              setImageUrl(res[0].url);
+              console.log("Files: ", res);
+              console.log("Uploaded Image URL:", res[0].url);
+              console.log(setImageUrl);
+              alert("Upload Completed");
+            }}
+            onUploadError={(error) => {
+              alert(`ERROR! ${error.message}`);
+            }}
+          />
+        )}
 
-          <h2 className="flex justify-center items-center py-2">Get your Attestation</h2>
-
-          <div className='flex justify-center items-center py-2'>
+          {/* <div className='flex justify-center items-center py-2'>
             <ReCAPTCHA sitekey={process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY!} onChange={setCaptcha} />
-          </div>
-          <div className="flex justify-center items-center py-2">
+          </div> */}
+          {/* <div className="flex justify-center items-center py-2">
             <button className="btn items-center" type="button" onClick={createAttestation}>
               Get your Attestation
+            </button> 
+          </div> */}
+          <div className="mt-6 flex justify-end justify-center space-x-4">
+            <button
+              className="px-4 py-2 w-1/5 text-sm font-medium text-white bg-black rounded-md shadow-sm hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              type="button"
+              onClick={handleNext}
+            >
+              Next
             </button>
-          </div>
-
-          <h2 className="flex justify-center items-center py-2">Your Attestation Uid</h2>
-          <div className="flex justify-center items-center py-2">
-            <textarea
-              className="textarea textarea-bordered w-3/5 h-4/5"
-              placeholder="Attestation Uid"
-              value={attestationUID}
-              readOnly
-            />
+            <button className="px-4 py-2 w-1/5 text-sm font-medium text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" type="button">
+              Cancel
+            </button>
+          
           </div>
         </form>
+        )}
+
+        {/* Right Column: Empty */}
+        <div className="w-1/4"></div>
       </div>
-      {renderModal()}
       <Footer />
     </div>
   );
