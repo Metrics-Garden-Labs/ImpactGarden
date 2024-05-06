0e6eccff3b5b070f69f6aa2a531504bb6cbb9403
Cheetah0x <niallcheetham@gmail.com>
2024-05-06 14:36:45 +0100

    include user image on card

--- app/searchUsers/userList.tsx
+++ app/searchUsers/userList.tsx
@@ -3,6 +3,7 @@
 import React from 'react';
 import Link from 'next/link';
 import { User } from '../../src/types';
+import Image from 'next/image';
 
 interface Props {
   users: User[];
@@ -29,6 +30,22 @@ export default function UserList({ users, query, filter }: Props) {
           <Link key={user.id} href={`/users/${encodeURIComponent(user.username)}`}>
           <div className="flex flex-col p-6 border justify-center items-center bg-white text-black border-gray-300 rounded-xl w-full h-60 shadow-lg"
           >
+            <div className="rounded-full bg-gray-300 w-32 h-32 flex items-center justify-center overflow-hidden mb-4">
+              {user.pfp_url ? (
+                <Image
+                  src={user.pfp_url}
+                  alt="Project Logo"
+                  width={128}
+                  height={128}
+                  className="object-cover w-full h-full"
+                />
+              ) : (
+                <div className="flex items-center justify-center text-gray-500">
+                  {/* Add optional placeholder content if needed */}
+                  No Logo
+                </div>
+              )}
+            </div> 
             <h3 className="mb-2 text-xl font-semibold">{user.username}</h3>
             <p>FID: {user.fid}</p>
             {/* Display user's attestations */}
