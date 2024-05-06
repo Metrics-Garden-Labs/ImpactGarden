7595fd7e83cabb8d8ab7048e3758b525beb9a5a6
Cheetah0x <niallcheetham@gmail.com>
2024-05-06 22:12:02 +0100

    yup

--- app/projectSignUp/page.tsx
+++ app/projectSignUp/page.tsx
@@ -23,6 +23,98 @@ import { FaGithub } from "react-icons/fa";
 import { BsGlobe2 } from "react-icons/bs";
 import { Project } from '@/src/types';
 
+interface ConfirmationSectionProps {
+  attestationUID: string;
+  attestationData: AttestationData;
+  imageUrl: string;
+  ecosystem: string;
+  setSelectedProject: (project: Project) => void;
+}
+
+// ConfirmationSection Component Implementation
+const ConfirmationSection: React.FC<ConfirmationSectionProps> = ({
+  attestationUID,
+  attestationData,
+  imageUrl,
+  ecosystem,
+  setSelectedProject
+}) => {
+  const user = {
+    fid: '',
+    username: '',
+    ethAddress: '',
+  };
+  if (!attestationUID) {
+    return null; // If no attestationUID, don't show this section
+  }
+  console.log('Selected Project:', attestationData, imageUrl, ecosystem, setSelectedProject);
+  useEffect(() => {
+    if (attestationUID) {
+      const project: Project = {
+        userFid: user.fid,
+        projectName: attestationData.projectName,
+        websiteUrl: attestationData.websiteUrl,
+        twitterUrl: attestationData.twitterUrl,
+        githubUrl: attestationData.githubURL,
+        logoUrl: imageUrl,
+        projectUid: attestationUID,
+        ecosystem: ecosystem,
+        // Add other relevant properties from the attestationData
+      };
+      setSelectedProject(project);
+    }
+  }, [attestationUID, attestationData, imageUrl, setSelectedProject]);
+
+  return (
+    <div className="w-full bg-white p-8 shadow-lg flex flex-col items-center mx-auto my-10 rounded-lg">
+      <Image
+        src="/star.png"
+        alt="Attestation Created"
+        width={100}
+        height={100}
+        className="object-contain mb-10 mt-10"
+      />
+      <h2 className="text-4xl font-bold mb-10">Attestation Created</h2>
+      <p className="text-lg mb-10">Your project has been successfully created.</p>
+      <div className="w-1/3 flex flex-col items-center shadow-2xl rounded p-8 mb-8">
+        {imageUrl ? (
+          <Image
+            src={imageUrl}
+            alt="Project Logo"
+            width={400}
+            height={400}
+            className="object-contain mt-15"
+          />
+        ) : (
+          <div className="w-48 h-48 bg-gray-300 rounded-md flex items-center justify-center">
+            {/* Optional placeholder content */}
+          </div>
+        )}
+        <h3 className="text-center mt-6 mb-6 font-semibold text-gray-500 text-2xl">
+          {attestationData.projectName || 'Project Name'}
+        </h3>
+        <div className="flex justify-center py-4 items-center">
+          <BsGlobe2 className="text-black mx-2 text-2xl" />
+          <FaXTwitter className="text-black mx-2 text-2xl" />
+          <FaGithub className="text-black mx-2 text-2xl" />
+        </div>
+      </div>
+      <p className="text-lg mt-8">Attestation UID:</p>
+      <p className="text-lg p-2 underline">{attestationUID}</p>
+
+      {/* comment this back in when i have the eas scan link for each project
+      <Link href={`/projects/${attestationUID}`} passHref>
+        <p className="text-lg p-2 underline">{attestationUID}</p>
+      </Link> */}
+      <Link href={`/projects/${attestationData.projectName}`} passHref className="pt-6">
+        <button className="mt-4 px-6 py-3 bg-black text-white rounded-md text-lg">
+          Visit your Project
+        </button>
+      </Link>
+    </div>
+  );
+};
+
 type AttestationData = {
   projectName: string;
   websiteUrl: string;
@@ -586,98 +678,4 @@ export default function ProjectSignUp() {
   );
 }
 
-interface ConfirmationSectionProps {
-  attestationUID: string;
-  attestationData: AttestationData;
-  imageUrl: string;
-  ecosystem: string;
-  setSelectedProject: (project: Project) => void;
-}
-
-// ConfirmationSection Component Implementation
-const ConfirmationSection: React.FC<ConfirmationSectionProps> = ({
-  attestationUID,
-  attestationData,
-  imageUrl,
-  ecosystem,
-  setSelectedProject
-}) => {
-  const user = {
-    fid: '',
-    username: '',
-    ethAddress: '',
-  };
-  if (!attestationUID) {
-    return null; // If no attestationUID, don't show this section
-  }
-  console.log('Selected Project:', attestationData, imageUrl, ecosystem, setSelectedProject);
-  useEffect(() => {
-    if (attestationUID) {
-      const project: Project = {
-        userFid: user.fid,
-        projectName: attestationData.projectName,
-        websiteUrl: attestationData.websiteUrl,
-        twitterUrl: attestationData.twitterUrl,
-        githubUrl: attestationData.githubURL,
-        logoUrl: imageUrl,
-        projectUid: attestationUID,
-        ecosystem: ecosystem,
-        // Add other relevant properties from the attestationData
-      };
-      setSelectedProject(project);
-    }
-  }, [attestationUID, attestationData, imageUrl, setSelectedProject]);
-
-  return (
-    <div className="w-full bg-white p-8 shadow-lg flex flex-col items-center mx-auto my-10 rounded-lg">
-      <Image
-        src="/star.png"
-        alt="Attestation Created"
-        width={100}
-        height={100}
-        className="object-contain mb-10 mt-10"
-      />
-      <h2 className="text-4xl font-bold mb-10">Attestation Created</h2>
-      <p className="text-lg mb-10">Your project has been successfully created.</p>
-      <div className="w-1/3 flex flex-col items-center shadow-2xl rounded p-8 mb-8">
-        {imageUrl ? (
-          <Image
-            src={imageUrl}
-            alt="Project Logo"
-            width={400}
-            height={400}
-            className="object-contain mt-15"
-          />
-        ) : (
-          <div className="w-48 h-48 bg-gray-300 rounded-md flex items-center justify-center">
-            {/* Optional placeholder content */}
-          </div>
-        )}
-        <h3 className="text-center mt-6 mb-6 font-semibold text-gray-500 text-2xl">
-          {attestationData.projectName || 'Project Name'}
-        </h3>
-        <div className="flex justify-center py-4 items-center">
-          <BsGlobe2 className="text-black mx-2 text-2xl" />
-          <FaXTwitter className="text-black mx-2 text-2xl" />
-          <FaGithub className="text-black mx-2 text-2xl" />
-        </div>
-      </div>
-      <p className="text-lg mt-8">Attestation UID:</p>
-      <p className="text-lg p-2 underline">{attestationUID}</p>
-
-      {/* comment this back in when i have the eas scan link for each project
-      <Link href={`/projects/${attestationUID}`} passHref>
-        <p className="text-lg p-2 underline">{attestationUID}</p>
-      </Link> */}
-      <Link href={`/projects/${attestationData.projectName}`} passHref className="pt-6">
-        <button className="mt-4 px-6 py-3 bg-black text-white rounded-md text-lg">
-          Visit your Project
-        </button>
-      </Link>
-    </div>
-  );
-};
-function setSelectedProject(project: Project) {
-  throw new Error('Function not implemented.');
-}
 
