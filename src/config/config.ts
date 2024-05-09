223d3fb60100d1039219db40eb8610f7b4215a56
Cheetah0x <niallcheetham@gmail.com>
2024-05-09 13:55:36 +0100

    url

--- src/config/config.ts
+++ src/config/config.ts
@@ -15,5 +15,5 @@ const { useGlobalState } = createGlobalState(initialState);
 
 export { useGlobalState };
 
-export const NEXT_PUBLIC_URL = "https://module3-nu.vercel.app";
+export const NEXT_PUBLIC_URL = "https://www.metricsgarden.xyz/";
 //export const NEXT_PUBLIC_URL = "http://localhost:3001";
