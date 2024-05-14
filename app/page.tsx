836e4d4b79dfa2702359de6ea7be5544eb39cad2
Cheetah0x <niallcheetham@gmail.com>
2024-05-14 13:15:54 +0200

    landing

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,28 @@ import Footer from "./components/footer";
 //the plan for this is to recreate the mockup that Mari sent in
 //add the connect wallet button to the top right of the screen. 
 
+// pages/framer-landing.js
+import React from 'react';
+
+const FramerLanding = () => {
+  return (
+    <div style={{ width: '100vw', height: '100vh' }}>
+      <iframe
+        src="https://artsy-app-411405.framer.app/"
+        style={{ width: '100%', height: '100%', border: 'none' }}
+        title="Framer Landing Page"
+      ></iframe>
+    </div>
+  );
+};
+
+
 export default function Home() {
   return (
     <>
     <main className="bg-white text-black">
       <Navbar />
-      <h1>This will be the landing page</h1>
+      <FramerLanding />
     </main>
     </>
   );
