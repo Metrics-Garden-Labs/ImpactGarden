9ee0519eefd495d358d0a55122fb03fd807c32ab
Cheetah0x <niallcheetham@gmail.com>
2024-05-21 19:23:51 +0200

    attestation spinner icon

--- app/components/attestationCreationModal.tsx
+++ app/components/attestationCreationModal.tsx
@@ -1,25 +1,21 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import SpinnerIcon from './spinnermgl/mglspinner'; 
+import Mgltree from './spinnermgl/mgltree';
 
 export default function AttestationCreationModal() {
-
-    return(
-    <div className="fixed inset-0 flex items-center rounded justify-center z-50 bg-black bg-opacity-50">
-          <div className="bg-white p-8 rounded-lg shadow-lg">
-            <Image  
-              src="/MGLIcon.png"
-              alt="Loading"
-              width={100}
-              height={100}
-              className='justify-center mx-auto object-contain mt-6 mb-6'
-            />
-            <h2 className="text-xl font-bold mb-4 justify-center text-center mt-6 mb-6">Processing Attestation</h2>
-            <div className="flex items-center">
-              <p>Please wait while your attestation is being processed...</p>
-            </div>
-            <div className="flex justify-center items-center mb-6 mt-6">
-              <p className="loading loading-spinner loading-lg text-center"></p>
-            </div>
-          </div>
+  return (
+    <div className="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+      <div className="bg-white p-8 rounded-lg shadow-lg">
+        <div className="flex justify-center items-center mb-6 mt-6">
+          <SpinnerIcon className="spinner w-16 h-16" />
+          <Mgltree className="absolute w-12 h-12" />
         </div>
-    )
+        <h2 className="text-xl font-bold mb-4 justify-center text-center mt-6 mb-6">Processing Attestation</h2>
+        <div className="flex items-center">
+          <p>Please wait while your attestation is being processed...</p>
+        </div>
+        
+      </div>
+    </div>
+  );
 }
---
+++ app/components/spinnermgl/mgltree.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Mgltree = (props:any) => (
+
+<svg width="66" height="67" viewBox="0 0 66 67" fill="none" xmlns="http://www.w3.org/2000/svg" className={props.className}>
+<path d="M65.2331 29.2741H41.7136L58.329 12.6587L53.0182 7.35624L36.4112 23.9632V0.468994H28.9001V23.9632L12.2932 7.35624L6.98234 12.6587L23.5977 29.2741H0.103516V36.7767H28.9001V66.2561H36.4112V36.7767H65.2331V29.2741Z" fill="#2C3F2D"/>
+</svg>
+);
+
+export default Mgltree;
