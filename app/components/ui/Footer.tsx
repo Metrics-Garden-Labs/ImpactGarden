3cd13927c807b70eeb502d95eea3da932df9d5e5
Cheetah0x <niallcheetham@gmail.com>
2024-08-26 13:33:23 +0100

    chore: twitter icon to the right of the footer

--- app/components/ui/Footer.tsx
+++ app/components/ui/Footer.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 export default function Footer() {
   return (
     <footer className="footer p-6 sm:p-8 md:p-12 bg-headerblack text-white">
-      <div className="flex flex-col md:flex-row justify-between items-center gap-8">
+      <div className="flex flex-col md:flex-row justify-between items-center w-full">
         <div className="flex flex-col md:flex-row items-center gap-4 md:gap-8">
           <button className="btn bg-headerblack text-xl border-none">
             <Image src="/mglwhite.png" alt="MGL Logo" width={12} height={12} className="h-12 w-12" />
@@ -23,10 +23,9 @@ export default function Footer() {
             </Link>
           </div>
         </div>
-        <div className="flex flex-1 justify-end items-center gap-2 sm:gap-8 mt-4 md:mt-0 pl-70">
+        <div className="mt-4 md:mt-0 md:ml-auto">
           <Link href="https://x.com/metricsgarden" target="_blank" className="flex items-center">
             <FaXTwitter className="text-white text-lg w-6 h-6 hover:text-[#2C3F2D]" />
-            <p className="ml-1 text-lg">Twitter</p>
           </Link>
         </div>
       </div>
