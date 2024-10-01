cea98528b8ead338b9e8ff636ec7b073e89846a3
Cheetah0x <niallcheetham@gmail.com>
2024-10-01 16:31:16 +0100

    fix: removed the ai functionality due to package changes

--- app/metricsdb/page.tsx
+++ app/metricsdb/page.tsx
@@ -1,5 +1,4 @@
 'use client'
-import AIChatButton from '../components/ai/aichatbutton'
 import React from 'react'
 import Footer from '@/app/components/ui/Footer'
 import ReviewCarousel from '../components/attestations/ReviewCarousel'
@@ -10,7 +9,7 @@ export default function page() {
   return (
     <main className='bg-white text-black'>
     <div className="h-screen flex items-center justify-center">Under Construction !!</div>
-      <AIChatButton />
+
     <Footer />
     
     </main>
