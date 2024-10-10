6dac318f597704c457aac95af433b78c4fbb46c3
dolvin17 <101130252+dolvin17@users.noreply.github.com>
2024-10-10 04:24:38 +0200

    Feature/new review modal (#12)
    * improving wording (#10)
    
    * adding className prop to customize component
    
    * refactor modal on two tabs, included reviewForm beside to description
    
    * adding fn
    
    * refactor modal contributions

--- app/components/attestations/governanceAttestationForms/GovernanceInfraToolingForm.tsx
+++ app/components/attestations/governanceAttestationForms/GovernanceInfraToolingForm.tsx
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
-import { RxCross2 } from 'react-icons/rx';
-import SmileyRatingScale from '@/app/components/ui/SmileyRatingScale';
-import RatingScale10 from '@/app/components/ui/RatingScale10';
-import { contributionRolesKey } from '@/src/types';
-import { getSmileyRatingLabel } from '@/src/utils/helpers';
-import { unknown } from 'zod';
+import React, { useState } from "react";
+import { RxCross2 } from "react-icons/rx";
+import SmileyRatingScale from "@/app/components/ui/SmileyRatingScale";
+import RatingScale10 from "@/app/components/ui/RatingScale10";
+import { contributionRolesKey } from "@/src/types";
+import { getSmileyRatingLabel } from "@/src/utils/helpers";
+import { unknown } from "zod";
+import { cn } from "@/src/lib/helpers";
 
 interface GovernanceInfraToolingFormProps {
   rating1: number;
@@ -15,6 +16,7 @@ interface GovernanceInfraToolingFormProps {
   setExtraFeedback: (extraFeedback: string) => void;
   onSubmit: (formData: any) => void;
   onClose: () => void;
+  className?: string;
 }
 
 const GovernanceInfraToolingForm: React.FC<GovernanceInfraToolingFormProps> = ({
@@ -26,6 +28,7 @@ const GovernanceInfraToolingForm: React.FC<GovernanceInfraToolingFormProps> = ({
   setExtraFeedback,
   onSubmit,
   onClose,
+  className,
 }) => {
   const [localRating1, setLocalRating1] = useState(rating1);
   const [localSmileyRating, setLocalSmileyRating] = useState(smileyRating);
@@ -38,10 +41,9 @@ const GovernanceInfraToolingForm: React.FC<GovernanceInfraToolingFormProps> = ({
     "The absence of this tool would significantly disrupt my work.",
   ];
 
-
   const handleSubmit = () => {
     //convert smiley rating to label
-    if (localSmileyRating === 0 || undefined ) {
+    if (localSmileyRating === 0 || undefined) {
       alert("Please leave rating, Neutral, Slighly Upset or Extremely Upset.");
       return; // Prevent form submission
     }
@@ -59,21 +61,23 @@ const GovernanceInfraToolingForm: React.FC<GovernanceInfraToolingFormProps> = ({
   };
 
   return (
-    <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center" onClick={onClose}>
+    <div
+      className={cn(
+        "fixed inset-0 bg-gray-600/60 flex justify-center items-center",
+        className
+      )}
+      onClick={onClose}
+    >
       <div
-        className="relative m-auto p-6 bg-white rounded-lg shadow-lg max-w-4xl w-3/4 md:w-1/2 lg:w-2/3 max-h-[90vh] overflow-y-auto mx-4 md:mx-20"
+        className="relative Content m-auto p-6 bg-white rounded-lg shadow-lg max-w-4xl w-3/4 md:w-1/2 lg:w-2/3 max-h-[90vh] overflow-y-auto mx-4 md:mx-20"
         onClick={(e) => e.stopPropagation()}
       >
         <>
           {/* Close Button */}
-          <button 
-            className="absolute top-4 right-4 text-gray-600 hover:text-gray-800" 
-            onClick={onClose}
-          >
-            <RxCross2 className="h-6 w-6" />
-          </button>
-
-          <h2 className="text-xl font-bold mb-4 text-center">Attest to Contribution</h2>
+          
+          <h2 className="text-xl font-bold mb-4 text-center">
+            ✨🔴 Complete your Review 🔴✨
+          </h2>
           {/* <div className="mb-4">
             <h3 className="font-semibold text-center">Please select the roles you perform within Optimism's Governance for which this contribution has been impactful. Select all that apply.</h3>
             <div className='font-semibold text-center mt-4'>
@@ -92,36 +96,44 @@ const GovernanceInfraToolingForm: React.FC<GovernanceInfraToolingFormProps> = ({
 
           {/* Q1 */}
           <div className="mb-6">
-            <h3 className='font-semibold text-center text-black'>How likely are you to recommend this contribution to someone in your role or position?</h3>
-            <RatingScale10 rating={localRating1} handleRating={setLocalRating1} />
+            <h3 className="font-semibold text-center text-black">
+              How likely are you to recommend this tool to someone in your role
+              or position?
+            </h3>
+            <RatingScale10
+              rating={localRating1}
+              handleRating={setLocalRating1}
+            />
           </div>
           <hr className="my-4" />
-
           {/* Q2 */}
           <div className="mb-6">
-            <h3 className='font-semibold text-center text-black'>How would you feel if this tool/contribution ceased to exist?</h3>
-            <SmileyRatingScale 
-              rating={localSmileyRating} 
-              handleRating={setLocalSmileyRating} 
+            <h3 className="font-semibold text-center text-black">
+              How would you feel if this tool/contribution ceased to exist?
+            </h3>
+            <SmileyRatingScale
+              rating={localSmileyRating}
+              handleRating={setLocalSmileyRating}
               additionalInfo={additionalInfo}
-              />
-
+            />
           </div>
           <hr className="my-4" />
-
           {/* <div className="mb-4">
             <h3 className='font-semibold text-center mt-4'>Has this contribution been useful for your day-to-day role?</h3>
             <RatingScale rating={rating2} handleRating={handleRating2}/>
           </div> */}
-
           {/* <div className="mb-4">
             <h3 className='font-semibold text-center mt-4'>Has this contribution been useful for increasing accessibility to your governance functions?</h3>
             <RatingScale rating={rating3} handleRating={handleRating3}/>
           </div> */}
-
           {/* Q3 */}
           <div className="mb-6">
-            <label className="block text-gray-700 font-bold mb-2 ">Please provide a brief explanation for your rating. For example, what aspects of this tool make it stand out from others, or what challenges do you face without it?</label>
+            <label className="block text-gray-700 font-bold mb-2 ">
+              Please provide a brief explanation for your rating.
+              <br />
+              For example, what aspects of this tool make it stand out from
+              others, or what challenges do you face without it?
+            </label>
             <textarea
               value={localFeedback}
               onChange={(e) => setLocalFeedback(e.target.value)}
@@ -129,27 +141,26 @@ const GovernanceInfraToolingForm: React.FC<GovernanceInfraToolingFormProps> = ({
               rows={4}
               maxLength={200}
             />
-            <div className="text-right text-xs text-gray-500 mt-1">{localFeedback.length}/200</div>
+            <div className="text-right text-xs text-gray-500 mt-1">
+              {localFeedback.length}/200
+            </div>
           </div>
           <hr className="my-4" />
-
           {/* Q4 */}
-          <div className="mb-6">
-            <label className="block text-gray-700 font-bold mb-2 text-black">Any additional feedback or suggestions on this contribution? This response will be confidential and only shared with the contributor.</label>
-            <textarea
-              value={localExtraFeedback}
-              onChange={(e) => setLocalExtraFeedback(e.target.value)}
-              className="w-full px-3 py-2 text-gray-700 border rounded-lg focus:outline-none"
-              rows={4}
-              maxLength={200}
-            />
-            <div className="text-right text-xs text-gray-500 mt-1">{localExtraFeedback.length}/200</div>
-          </div>
-          <hr className="my-4" />
-
+     
           <div className="text-center py-3">
-            <button className='btn bg-headerblack text-white hover:bg-blue-500 mr-2' onClick={onClose}>Back</button>
-            <button className="btn bg-headerblack text-white hover:bg-blue-500" onClick={handleSubmit}>Send Review</button>
+            <button
+              className="btn bg-headerblack text-white hover:bg-blue-500 mr-2"
+              onClick={onClose}
+            >
+              Back
+            </button>
+            <button
+              className="btn bg-headerblack text-white hover:bg-blue-500"
+              onClick={handleSubmit}
+            >
+              Send Review
+            </button>
           </div>
         </>
       </div>
--- app/components/searchProjects/ProjectPageClient.tsx
+++ app/components/searchProjects/ProjectPageClient.tsx
@@ -1,11 +1,14 @@
-'use client';
-import React, { useState, useCallback, useMemo, useEffect } from 'react';
-import SearchProjects from './searchProjects';
-import ProjectList from './projectList1';
-import { ContributionWithProjectsAndAttestationCount, Project, ProjectCount, SearchResult } from '../../../src/types';
-import { NEXT_PUBLIC_URL } from "../../../src/config/config";
-import { getContributions } from './actions';
-import ContributionList from './ContributionList';
+"use client";
+import React, { useState, useCallback, useMemo, useEffect } from "react";
+import SearchProjects from "./searchProjects";
+import {
+  ContributionWithProjectsAndAttestationCount,
+  Project,
+  ProjectCount,
+  SearchResult,
+} from "../../../src/types";
+import { getContributions, getProjectByForCategories } from "./actions";
+import ContributionList from "./ContributionList";
 
 interface Props {
   projects: (Project | ProjectCount)[];
@@ -16,76 +19,67 @@ interface Props {
   error?: string;
 }
 
-const ProjectPageClient = ({ projects: initialProjects, query: initialQuery, filter: initialFilter, sortOrder: initialSortOrder, searchResults: initialSearchResults, error }: Props) => {
-  const [projects, setProjects] = useState<(Project | ProjectCount)[]>(initialProjects);
-  const [searchResults, setSearchResults] = useState<SearchResult[]>(initialSearchResults);
+const ProjectPageClient = ({
+  projects: initialProjects,
+  query: initialQuery,
+  filter: initialFilter,
+  sortOrder: initialSortOrder,
+  searchResults: initialSearchResults,
+  error,
+}: Props) => {
+  const [projects, setProjects] =
+    useState<(Project | ProjectCount)[]>(initialProjects);
+  const [searchResults, setSearchResults] =
+    useState<SearchResult[]>(initialSearchResults);
   const [filter, setFilter] = useState(initialFilter || "");
   const [sortOrder, setSortOrder] = useState(initialSortOrder || "A-Z");
   const [query, setQuery] = useState(initialQuery);
-  const [contributions, setContributions] = useState<ContributionWithProjectsAndAttestationCount[]>([]);
+  const [contributions, setContributions] = useState<
+    ContributionWithProjectsAndAttestationCount[]
+  >([]);
 
   useEffect(() => {
     fetchContributions(query, filter, sortOrder);
   }, [query, filter, sortOrder]);
 
+  const fetchContributions = useCallback(
+    async (newQuery: string, newFilter: string, newSortOrder: string) => {
+      try {
+        const [category, subcategory] = newFilter.split(":");
+        console.log("Fetching contributions with:", {
+          newQuery,
+          category,
+          subcategory,
+          newSortOrder,
+        });
 
-  // const uniqueProjects = useMemo(() => {
-  //   const projectMap = new Map<string, Project | ProjectCount>();
-  //   projects.forEach(project => {
-  //     if (!projectMap.has(project.projectName) || 
-  //         ('attestationCount' in project && 
-  //          (!('attestationCount' in projectMap.get(project.projectName)!) || 
-  //           project.attestationCount > (projectMap.get(project.projectName) as ProjectCount).attestationCount))) {
-  //       projectMap.set(project.projectName, project);
-  //     }
-  //   });
-  //   console.log("uniqueProjects:", Array.from(projectMap.values()).slice(0, 5));
-  //   return Array.from(projectMap.values());
-  // }, [projects]);
+        const contributions = await getContributions(
+          category,
+          subcategory,
+          newSortOrder
+        );
 
-  // const fetchProjects = useCallback(async (newQuery: string, newFilter: string, newSortOrder: string) => {
-  //   try {
-  //     const [category, subcategory] = newFilter.split(':');
-  //     const response = await fetch(`${NEXT_PUBLIC_URL}/api/getProjects`, {
-  //       method: 'POST',
-  //       headers: { 'Content-Type': 'application/json' },
-  //       body: JSON.stringify({
-  //         category,
-  //         subcategory,
-  //         sortOrder: newSortOrder,
-  //       }),
-  //     });
+        const projects: Project[] = await getProjectByForCategories(
+          category,
+          subcategory
+        );
 
-  //     if (response.ok) {
-  //       const data = await response.json();
-  //       console.log("API response for projects:", data.projects.slice(0, 5));
-  //       setProjects(data.projects);
-  //       setSearchResults(data.searchResults || []);
-  //     } else {
-  //       console.error('Error fetching data');
-  //     }
-  //   } catch (error) {
-  //     console.error('Error during fetch operation:', error);
-  //   }
-  // }, []);
+        console.log(
+          "Server action response for contributions:",
+          contributions.slice(0, 5)
+        );
 
+        setProjects(projects);
 
-  
-  const fetchContributions = useCallback(async (newQuery: string, newFilter: string, newSortOrder: string) => {
-    try {
-      const [category, subcategory] = newFilter.split(':');
-      console.log('Fetching contributions with:', { newQuery, category, subcategory, newSortOrder });
-      
-      const contributions = await getContributions(category, subcategory, newSortOrder);
-      
-      console.log("Server action response for contributions:", contributions.slice(0, 5));
-      setContributions(contributions as unknown as ContributionWithProjectsAndAttestationCount[]);
-      
-    } catch (error) {
-      console.error('Error fetching contributions:', error);
-    }
-  }, [getContributions]);
-  
+        setContributions(
+          contributions as unknown as ContributionWithProjectsAndAttestationCount[]
+        );
+      } catch (error) {
+        console.error("Error fetching contributions:", error);
+      }
+    },
+    [getContributions]
+  );
 
   const handleSearchChange = (newQuery: string) => {
     setQuery(newQuery);
@@ -107,7 +101,7 @@ const ProjectPageClient = ({ projects: initialProjects, query: initialQuery, fil
 
   return (
     <div className="bg-white text-black">
-      <SearchProjects 
+      <SearchProjects
         onSearchChange={handleSearchChange}
         onFilterChange={handleFilterChange}
         onSortOrderChange={handleSortOrderChange}
@@ -119,16 +113,14 @@ const ProjectPageClient = ({ projects: initialProjects, query: initialQuery, fil
           <p>{error}</p>
         </div>
       ) : (
-        // <ProjectList
-        //   projects={projects}
-        //   query={query}
-        //   filter={filter}
-        //   sortOrder={sortOrder}
-        //   searchResults={searchResults}
-        // />
         <ContributionList
           contributions={contributions}
           query={query}
+          project={
+            projects.find((project) =>
+              contributions.some((c) => c.projectUid === project.projectUid)
+            ) as any
+          }
           filter={filter}
           sortOrder={sortOrder}
         />
