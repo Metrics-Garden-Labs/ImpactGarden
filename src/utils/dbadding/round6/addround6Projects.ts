dfb83451f754cc9c07ab6f64c44ec8b951894291
Cheetah0x <niallcheetham@gmail.com>
2024-10-11 12:14:54 +0100

    updated project scripts

--- src/utils/dbadding/round6/addround6Projects.ts
+++ src/utils/dbadding/round6/addround6Projects.ts
@@ -34,7 +34,7 @@ const client = new NeynarAPIClient(NEYNAR_API_KEY); // Initialize NeynarAPIClien
 
 const addProjectsToDB = async () => {
   try {
-    const filePath = path.join(__dirname, "Round6Projects4.json");
+    const filePath = path.join(__dirname, "Round6Projects5.json");
     const jsonData = fs.readFileSync(filePath, "utf-8");
     const projectsData = JSON.parse(jsonData);
 
