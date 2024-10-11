a27a7eac46fe484e74784ce71db1fbb8be7e991d
dolvin17 <huertas876@gmail.com>
2024-10-11 03:50:48 +0200

    <Partial> props to fit on components

--- app/components/ui/RatingScale10.tsx
+++ app/components/ui/RatingScale10.tsx
@@ -1,17 +1,36 @@
-import React from 'react';
-import { RatingScaleProps } from '@/src/types';
+import React from "react";
+import { RatingScaleProps } from "@/src/types";
 
-const RatingScale10: React.FC<RatingScaleProps> = ({ rating, handleRating }) => {
+const RatingScale10: React.FC<Partial<RatingScaleProps>> = ({
+  rating,
+  handleRating,
+}) => {
   const colors = [
-    'bg-red-200', 'bg-red-200', 'bg-red-200', 'bg-orange-200', 'bg-orange-200',
-    'bg-yellow-200', 'bg-yellow-200', 'bg-yellow-200', 'bg-green-200', 'bg-green-200',
-    'bg-green-200'
+    "bg-red-200",
+    "bg-red-200",
+    "bg-red-200",
+    "bg-orange-200",
+    "bg-orange-200",
+    "bg-yellow-200",
+    "bg-yellow-200",
+    "bg-yellow-200",
+    "bg-green-200",
+    "bg-green-200",
+    "bg-green-200",
   ];
 
   const brightColors = [
-    'bg-red-600', 'bg-red-500', 'bg-red-400', 'bg-orange-400', 'bg-orange-300',
-    'bg-yellow-300', 'bg-yellow-400', 'bg-yellow-500', 'bg-green-400', 'bg-green-500',
-    'bg-green-600'
+    "bg-red-600",
+    "bg-red-500",
+    "bg-red-400",
+    "bg-orange-400",
+    "bg-orange-300",
+    "bg-yellow-300",
+    "bg-yellow-400",
+    "bg-yellow-500",
+    "bg-green-400",
+    "bg-green-500",
+    "bg-green-600",
   ];
 
   return (
@@ -22,10 +41,8 @@ const RatingScale10: React.FC<RatingScaleProps> = ({ rating, handleRating }) => 
             key={num}
             className={`w-10 h-10 mx-1 flex items-center justify-center cursor-pointer rounded-full text-white ${
               rating === num ? brightColors[num] : colors[num]
-            } ${
-              rating === num ? 'ring-2 ring-offset-2 ring-gray-500' : ''
-            }`}
-            onClick={() => handleRating(num)}
+            } ${rating === num ? "ring-2 ring-offset-2 ring-gray-500" : ""}`}
+            onClick={() => handleRating?.(num)}
           >
             {num}
           </div>
