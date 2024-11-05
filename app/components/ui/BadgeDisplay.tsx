db015132f24f2806dca3e280a1b8baed1ff0631d
dolvin17 <huertas876@gmail.com>
2024-11-05 02:56:39 +0100

    feat: exporting BadgeDisplayProps

--- app/components/ui/BadgeDisplay.tsx
+++ app/components/ui/BadgeDisplay.tsx
@@ -1,7 +1,7 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
-interface BadgeDisplayProps {
+export interface BadgeDisplayProps {
   isCoinbaseVerified?: boolean;
   isOpBadgeholder?: boolean;
   isPowerBadgeholder?: boolean;
@@ -14,18 +14,19 @@ export const BadgeDisplay: React.FC<BadgeDisplayProps> = ({
   isOpBadgeholder,
   isPowerBadgeholder,
   isDelegate,
-  s4Participant
+  s4Participant,
 }) => {
-   
-
-
-
-
   return (
     <span className="flex items-center space-x-1 ml-2">
       {isCoinbaseVerified && (
         <span className="tooltip" data-tip="Coinbase Verified Wallet">
-          <Image src="/coinbaseWallet.png" alt="Coinbase Wallet Badge" width={20} height={20} style={{ height: "auto" }} />
+          <Image
+            src="/coinbaseWallet.png"
+            alt="Coinbase Wallet Badge"
+            width={20}
+            height={20}
+            style={{ height: "auto" }}
+          />
         </span>
       )}
       {isOpBadgeholder && (
@@ -35,17 +36,35 @@ export const BadgeDisplay: React.FC<BadgeDisplayProps> = ({
       )}
       {isPowerBadgeholder && (
         <span className="tooltip" data-tip="Warpcast Power User">
-          <Image src="/powerBadge.png" alt="Warpcast Power Badge" width={20} height={20} style={{ height: "auto" }} />
+          <Image
+            src="/powerBadge.png"
+            alt="Warpcast Power Badge"
+            width={20}
+            height={20}
+            style={{ height: "auto" }}
+          />
         </span>
       )}
       {isDelegate && (
         <span className="tooltip" data-tip="Optimism Delegate">
-          <Image src="/opDelegate.png" alt="Optimism Delegate Badge" width={20} height={20} style={{ height: "auto" }} />
+          <Image
+            src="/opDelegate.png"
+            alt="Optimism Delegate Badge"
+            width={20}
+            height={20}
+            style={{ height: "auto" }}
+          />
         </span>
       )}
       {s4Participant && (
         <span className="tooltip" data-tip="Season 4 Participant">
-          <Image src="/s-4grantparticipants.png" alt="Season 4 Participant Badge" width={20} height={20} style={{ height: "auto" }}/>
+          <Image
+            src="/s-4grantparticipants.png"
+            alt="Season 4 Participant Badge"
+            width={20}
+            height={20}
+            style={{ height: "auto" }}
+          />
         </span>
       )}
     </span>
