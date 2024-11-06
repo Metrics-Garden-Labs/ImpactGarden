a649872ba4f20c86a681c4bdd9a5df181f88005d
dolvin17 <huertas876@gmail.com>
2024-11-06 04:03:21 +0100

    chores:ui-userList

--- app/components/searchUsers/userList.tsx
+++ app/components/searchUsers/userList.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import Link from 'next/link';
-import { User } from '@/src/types';
-import Image from 'next/image';
-import { getUserAddressesByFid } from '@/src/lib/db/dbusers';
-import { BadgeDisplay } from '@/app/components/ui/BadgeDisplay';
-import { getUserBadgeStatus } from '@/src/utils/badges/badgeHelper';
-
+import React from "react";
+import Link from "next/link";
+import { User } from "@/src/types";
+import Image from "next/image";
+import { getUserAddressesByFid } from "@/src/lib/db/dbusers";
+import { BadgeDisplay } from "@/app/components/ui/BadgeDisplay";
+import { getUserBadgeStatus } from "@/src/utils/badges/badgeHelper";
 
 interface Props {
   users: User[];
@@ -14,38 +13,53 @@ interface Props {
   verificationFilter: string; // Add this prop
 }
 
-export default async function UserList({ users, query, filter, verificationFilter }: Props) {
+export default async function UserList({
+  users,
+  query,
+  filter,
+  verificationFilter,
+}: Props) {
   const filteredUsers = [];
 
   for (const user of users) {
-    if (user.fid === '9999999') {
+    if (user.fid === "9999999") {
       continue; // this should skip and not display the example user
     }
 
-    const { isCoinbaseVerified, isOpBadgeholder, isPowerBadgeholder, isDelegate, s4Participant } = await getUserBadgeStatus(user.fid);
+    const {
+      isCoinbaseVerified,
+      isOpBadgeholder,
+      isPowerBadgeholder,
+      isDelegate,
+      s4Participant,
+    } = await getUserBadgeStatus(user.fid);
 
     let matchesFilter = true;
 
-    if (filter === 'username') {
-      matchesFilter = (user.username?.toLowerCase() || '').includes(query.toLowerCase());
-    } else if (filter === 'fid') {
-      matchesFilter = (user.fid?.toLowerCase() || '').includes(query.toLowerCase());
+    if (filter === "username") {
+      matchesFilter = (user.username?.toLowerCase() || "").includes(
+        query.toLowerCase()
+      );
+    } else if (filter === "fid") {
+      matchesFilter = (user.fid?.toLowerCase() || "").includes(
+        query.toLowerCase()
+      );
     }
 
     if (matchesFilter) {
-      if (verificationFilter === 'coinbaseVerified' && !isCoinbaseVerified) {
+      if (verificationFilter === "coinbaseVerified" && !isCoinbaseVerified) {
         continue;
       }
-      if (verificationFilter === 'opBadgeholder' && !isOpBadgeholder) {
+      if (verificationFilter === "opBadgeholder" && !isOpBadgeholder) {
         continue;
       }
-      if (verificationFilter === 'powerBadgeholder' && !isPowerBadgeholder) {
+      if (verificationFilter === "powerBadgeholder" && !isPowerBadgeholder) {
         continue;
       }
-      if (verificationFilter === 'delegate' && !isDelegate) {
+      if (verificationFilter === "delegate" && !isDelegate) {
         continue;
       }
-      if (verificationFilter === 's4Participant' && !s4Participant) {
+      if (verificationFilter === "s4Participant" && !s4Participant) {
         continue;
       }
       filteredUsers.push({
@@ -54,23 +68,32 @@ export default async function UserList({ users, query, filter, verificationFilte
         isOpBadgeholder,
         isPowerBadgeholder,
         isDelegate,
-        s4Participant
+        s4Participant,
       });
     }
   }
 
   return (
-    <div className="p-6 bg-white mx-auto gap-12 max-w-6xl">
-      <div className="grid grid-cols-1 gap-4 mx-3 sm:grid-cols-2 sm:gap-4 sm:mx-3 md:grid-cols-3 md:mx-8 md:mx-8 lg:grid-cols-4 lg:gap-12 max-w-6xl overflow-y-auto">
+    <div className="p-6 mx-auto gap-8 max-w-6xl rounded-lg">
+      <div className="grid grid-cols-1 gap-4 mx-3 sm:grid-cols-2 sm:gap-4  md:grid-cols-3 lg:grid-cols-4 lg:gap-12 max-w-6xl overflow-y-auto  overflow-x-hidden">
         {filteredUsers.map((user) => (
-          <Link key={user.id} href={`/users/${encodeURIComponent(user.username)}`}>
-            <div className="flex flex-col p-6 border justify-center items-center bg-white text-black border-lime-900/30 rounded-2xl w-full h-60 shadow-lg">
-              <div className="rounded-full bg-gray-300 w-32 h-32 flex items-center justify-center overflow-hidden mb-4">
+          <Link
+            key={user.id}
+            href={`/users/${encodeURIComponent(user.username)}`}
+          >
+            <div className="flex flex-col p-6 border justify-center items-center bg-white text-black rounded-lg shadow-lg hover:shadow-xl transition-shadow cursor-pointer hover:bg-[#F4D3C3]/20 border-lime-900/30 w-full h-60">
+              <div className="rounded-full w-32 h-32 flex shadow-black/50 shadow-md hover:shadow-md items-center justify-center overflow-hidden mb-4">
                 {user.pfp_url ? (
-                  <Image src={user.pfp_url} alt="Profile Picture" width={128} height={128} className="object-cover w-full h-full" />
+                  <Image
+                    src={user.pfp_url}
+                    alt="Profile Picture"
+                    width={128}
+                    height={128}
+             className="object-cover w-full h-full shrink-0 border-4 border-[#F4D3C3]  rounded-full transition-transform duration-300 transform hover:scale-110"
+                  />
                 ) : (
-                  <div className="flex items-center justify-center text-gray-500">
-                    No Logo
+                  <div className="flex items-center  tracking-widest  bg-white justify-center text-black">
+                    No pfp
                   </div>
                 )}
               </div>
