711224c354965101d9370f5eea60e29d0e98646c
dolvin17 <huertas876@gmail.com>
2025-05-04 20:57:23 +0200

    deployment with middleware

---
+++ middleware.ts
@@ -0,0 +1,14 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const host = request.headers.get("host") || "";
+  const url = request.nextUrl.clone();
+
+  if (host.startsWith("opimpact.metricsgarden.xyz")) {
+    url.pathname = `/impact-framework${url.pathname}`;
+    return NextResponse.rewrite(url);
+  }
+
+  return NextResponse.next();
+}
