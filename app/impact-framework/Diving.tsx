7868a7b08a0e5b6cff0fd785f755e85c6d50f5f2
dolvin17 <huertas876@gmail.com>
2025-05-05 01:40:33 +0200

    hotfix: improving responsive

--- app/impact-framework/Diving.tsx
+++ app/impact-framework/Diving.tsx
@@ -3,23 +3,23 @@ import { FaRegArrowAltCircleRight } from "react-icons/fa";
 
 export default function Diving() {
   return (
-    <div className="relative w-full py-8 overflow-hidden max-h-screen">
+    <div className="relative h-64 mt-8 sm:h-80 lg:h-auto w-full overflow-hidden max-h-screen">
       <Image
         src="/bg-retropgf2.png"
         alt="background"
         width={1920}
         height={1080}
-        className="object-cover w-full h-auto max-h-screen"
+        className="object-cover size-full max-h-screen"
         priority
       />
-      <div className="absolute top-16 left-4 md:left-12 lg:left-24 z-10">
+      <div className="absolute top-4 sm:top-10 left-4 md:left-12 lg:left-24 z-10">
         <h1 className="text-white text-xl sm:text-2xl md:text-3xl font-bold">
           RetroPGF3
         </h1>
         <div className="w-8 bg-white h-[2px] my-2" />
       </div>
-      <div className="absolute top-1/2 left-1/2 z-0 flex flex-col -translate-x-1/2 -translate-y-1/2 px-4 sm:px-8">
-        <div className="flex flex-row gap-8">
+      <div className="absolute w-full sm:top-1/2  z-0 flex flex-col top-20  sm:-translate-y-1/2 px-4 sm:px-8">
+        <div className="flex flex-row justify-center sm:gap-8 w-full gap-4">
           <Image
             src="/retropgf_sun.svg"
             alt="sun"
@@ -33,7 +33,7 @@ export default function Diving() {
             alt="sun"
             width={0}
             height={0}
-          className="w-16  md:w-32 h-auto mb-4 sm:mb-0"
+            className="w-16  md:w-32 h-auto mb-4 sm:mb-0"
           />
 
           <Image
@@ -52,8 +52,8 @@ export default function Diving() {
             className="w-16  md:w-32 h-auto mb-4 sm:mb-0"
           />
         </div>
-        <h1 className="text-white text-xl mt-8 md:text-5xl text-center font-bold leading-none">
-          Let's dive in!
+		<h1 className="text-white whitespace-nowrap text-xl sm:text-2xl md:text-4xl mt-4 text-center font-bold leading-none">
+		Let's dive in!
         </h1>
       </div>
       <div className="absolute bottom-6 right-6 sm:bottom-10 sm:right-20 z-20">
