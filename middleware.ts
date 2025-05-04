63b93e8ded1a25614900659eb375be7709feedfb
dolvin17 <huertas876@gmail.com>
2025-05-04 21:07:54 +0200

    hotfix

--- middleware.ts
+++ middleware.ts
@@ -5,9 +5,13 @@ export function middleware(request: NextRequest) {
   const host = request.headers.get("host") || "";
   const url = request.nextUrl.clone();
 
-  if (host.startsWith("opimpact.metricsgarden.xyz")) {
-    url.pathname = `/impact-framework${url.pathname}`;
-    return NextResponse.rewrite(url);
+  // Redirect if the host is opimpact.metricsgarden.xyz
+  if (
+    host.startsWith("opimpact.metricsgarden.xyz") &&
+    url.pathname !== "/impact-framework"
+  ) {
+    url.pathname = "/impact-framework"; // redirect to /impact-framework
+    return NextResponse.redirect(url);
   }
 
   return NextResponse.next();
