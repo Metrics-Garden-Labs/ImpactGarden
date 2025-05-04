81a884c90e55bfde74e2c9d8cdcd7ba8611a405a
dolvin17 <huertas876@gmail.com>
2025-05-04 21:18:50 +0200

    2try

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,16 @@
 import { NextResponse } from "next/server";
+
 import type { NextRequest } from "next/server";
 
 export function middleware(request: NextRequest) {
   const host = request.headers.get("host") || "";
+
   const url = request.nextUrl.clone();
 
-  // Redirect if the host is opimpact.metricsgarden.xyz
-  if (
-    host.startsWith("opimpact.metricsgarden.xyz") &&
-    url.pathname !== "/impact-framework"
-  ) {
-    url.pathname = "/impact-framework"; // redirect to /impact-framework
-    return NextResponse.redirect(url);
+  if (host.startsWith("opimpact.metricsgarden.xyz")) {
+    url.pathname = `/impact-framework${url.pathname}`;
+
+    return NextResponse.rewrite(url);
   }
 
   return NextResponse.next();
